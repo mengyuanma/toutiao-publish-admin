d88e28a368cebe15946ba31024cd33c9362ea341
mengyuan <2796905540@qq.com>
2020-12-17 16:34:49 +0800

    用户登录-创建页面组件

---
+++ src/views/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login-container">登录页面</div>
+</template>
+
+<script>
+export default {
+  name: "LoginIndex",
+  components: {},
+  props: {},
+  data: {},
+  component: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
